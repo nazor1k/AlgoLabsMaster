24fbf2e1b7f7ef220ae516689deedcf29607601e
Назар Коробчук <nazarkorobchuk08@gmail.com>
2026-04-20 23:27:30 +0300

    Lab 14: benchmark existing vs non-existing items

--- Lab14/main.cpp
+++ Lab14/main.cpp
@@ -28,50 +28,47 @@ int binarySearch(const vector<int>& arr, int target, long long& comparisons) {
     return -1;
 }
 
-int main() {
-    srand(time(0));
-    vector<int> sizes = { 100, 1000, 10000, 100000 };
+void printTable(const vector<int>& sizes, bool itemExists) {
+    if (itemExists) cout << "\n--- TEST 1: Searching for EXISTING item ---\n";
+    else cout << "\n--- TEST 2: Searching for NON-EXISTING item (-1) ---\n";
 
-    cout << "Size\tAlgorithm (Status)\tComparisons\tTime (ms) [Sort + Search = Total]\n";
-    cout << "----------------------------------------------------------------------------------\n";
+    cout << "Size\tAlgorithm\tComparisons\tTime (ms) [Sort + Search = Total]\n";
+    cout << "--------------------------------------------------------------------------\n";
 
     for (int n : sizes) {
         vector<int> arr(n);
         for (int i = 0; i < n; i++) arr[i] = rand() % 1000000;
-        int targetFound = arr[n / 2];
-        int targetNotFound = -1;
+        
+        int target = itemExists ? arr[n / 2] : -1;
         long long cmp;
 
         auto s1 = chrono::high_resolution_clock::now();
-        linearSearch(arr, targetNotFound, cmp);
+        linearSearch(arr, target, cmp);
         auto e1 = chrono::high_resolution_clock::now();
-        double tL1 = chrono::duration<double, milli>(e1 - s1).count();
-        cout << n << "\tLinear (Not Found)\t" << cmp << "\t\t0.00 + " << tL1 << " = " << tL1 << "\n";
+        double tLin = chrono::duration<double, milli>(e1 - s1).count();
+        cout << n << "\tLinear\t\t" << cmp << "\t\t0.00 + " << tLin << " = " << tLin << "\n";
 
-        auto s2 = chrono::high_resolution_clock::now();
-        linearSearch(arr, targetFound, cmp);
-        auto e2 = chrono::high_resolution_clock::now();
-        double tL2 = chrono::duration<double, milli>(e2 - s2).count();
-        cout << "\tLinear (Found)\t\t" << cmp << "\t\t0.00 + " << tL2 << " = " << tL2 << "\n";
-
-        auto ss = chrono::high_resolution_clock::now();
+        auto s_sort = chrono::high_resolution_clock::now();
         sort(arr.begin(), arr.end());
-        auto es = chrono::high_resolution_clock::now();
-        double tSort = chrono::duration<double, milli>(es - ss).count();
+        auto e_sort = chrono::high_resolution_clock::now();
+        double tSort = chrono::duration<double, milli>(e_sort - s_sort).count();
 
-        auto s3 = chrono::high_resolution_clock::now();
-        binarySearch(arr, targetNotFound, cmp);
-        auto e3 = chrono::high_resolution_clock::now();
-        double tB1 = chrono::duration<double, milli>(e3 - s3).count();
-        cout << "\tBinary (Not Found)\t" << cmp << "\t\t" << tSort << " + " << tB1 << " = " << (tSort + tB1) << "\n";
+        auto s_bin = chrono::high_resolution_clock::now();
+        binarySearch(arr, target, cmp);
+        auto e_bin = chrono::high_resolution_clock::now();
+        double tBin = chrono::duration<double, milli>(e_bin - s_bin).count();
 
-        auto s4 = chrono::high_resolution_clock::now();
-        binarySearch(arr, targetFound, cmp);
-        auto e4 = chrono::high_resolution_clock::now();
-        double tB2 = chrono::duration<double, milli>(e4 - s4).count();
-        cout << "\tBinary (Found)\t\t" << cmp << "\t\t" << tSort << " + " << tB2 << " = " << (tSort + tB2) << "\n";
-        cout << "----------------------------------------------------------------------------------\n";
+        cout << "\tBinary\t\t" << cmp << "\t\t" << tSort << " + " << tBin << " = " << (tSort + tBin) << "\n";
+        cout << "--------------------------------------------------------------------------\n";
     }
+}
+
+int main() {
+    srand(time(0));
+    vector<int> sizes = { 100, 1000, 10000, 100000 };
+
+    printTable(sizes, true);
+    printTable(sizes, false);
 
     return 0;
 }
