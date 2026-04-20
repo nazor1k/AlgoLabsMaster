1032eb098646108f2938b3a8bd93a27083e18599
Назар Коробчук <nazarkorobchuk08@gmail.com>
2026-04-20 17:14:41 +0300

    Remove comments from Lab 7

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,5 +1,3 @@
-// Lab7/main.cpp
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -40,7 +38,7 @@ void insertionSort(vector<int>& arr, long long& checks, long long& changes) {
             changes++;
             j--;
         }
-        if (j >= 0) checks++; // Last check that failed the while condition
+        if (j >= 0) checks++;
         arr[j + 1] = element;
     }
 }
@@ -50,14 +48,12 @@ void bucketSort(vector<int>& arr, int maxVal, int bucketCount, long long& checks
     int bucketSize = (maxVal / bucketCount) + 1;
     vector<vector<int>> buckets(bucketCount);
 
-    // Distribution
     for (int x : arr) {
         int k = x / bucketSize;
         if (k >= bucketCount) k = bucketCount - 1;
         buckets[k].push_back(x);
     }
 
-    // Sort each bucket and merge
     arr.clear();
     for (int k = 0; k < bucketCount; k++) {
         if (!buckets[k].empty()) {
@@ -95,7 +91,6 @@ int main() {
     for (int n : rozmiry) {
         vector<int> original = generateArray(n);
 
-        // Counting Sort
         vector<int> v1 = original;
         long long checks1 = 0;
         long long changes1 = 0;
@@ -106,7 +101,6 @@ int main() {
         
         cout << n << "\t| Counting\t| " << checks1 << "\t\t| " << changes1 << "\t\t| " << ns1 << "\n";
 
-        // Bucket Sort
         vector<int> v2 = original;
         long long checks2 = 0;
         long long changes2 = 0;
